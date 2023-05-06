389dfa2ed2411994a80abeaa16a24fadfd590571
jammy25 <kishan95.jammy@gmail.com>
2023-05-06 07:36:49 +0530

    delete button activated

--- Note Taking Website/JS/app.js
+++ Note Taking Website/JS/app.js
@@ -19,6 +19,7 @@ addBtn.addEventListener('click', function (e) {
     showNotes()
 })
 
+// Function to show elements from local storage
 function showNotes() {
     let notes = localStorage.getItem("notes");
     if (notes == null) {
@@ -34,11 +35,11 @@ function showNotes() {
                 <div class="card-body">
                     <h5 class="card-title">Note ${index + 1}</h5>
                     <p class="card-text">${element}</p>
-                    <button href="#" class="btn btn-primary">Delete Note</a>
+                    <button id="${index}" onclick="deleteNote(this.id)" class="btn btn-primary">Delete Note</button>
                 </div>
         </div>`;
-
     });
+    
     let notesElm = document.getElementById('notes');
     if (notesObj.length != 0) {
         notesElm.innerHTML = html;
@@ -46,4 +47,20 @@ function showNotes() {
     else {
         notesElm.innerHTML = `Nothing to show! Use "Add a Note" section above to add notes`
     }
+}
+
+// Function to delete a note
+function deleteNote(index){
+    console.log(`I am deleting`, index);
+
+    let notes = localStorage.getItem("notes");
+    if (notes == null) {
+        notesObj = [];
+    }
+    else{
+        notesObj = JSON.parse(notes);
+    }
+    notesObj.splice(index, 1);
+    localStorage.setItem("notes", JSON.stringify(notesObj));
+    showNotes();
 }
